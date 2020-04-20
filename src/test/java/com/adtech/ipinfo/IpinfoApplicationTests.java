99e5f2589f82b1cb45951c7c418ab25877a1de07
Abhit Kumar Das <abhitk@obsessory.com>
2020-04-20 16:51:01 +0530

    initial commit

---
+++ src/test/java/com/adtech/ipinfo/IpinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adtech.ipinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
